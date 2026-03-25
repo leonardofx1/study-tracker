984f0d03b3d3f3281138094fb8ab97b7038e8c61
leonardofx1 <leonardonunesfreitas1@gmail.com>
2026-03-25 05:40:41 -0300

    session schema

--- src/schemasZod/session.ts
+++ src/schemasZod/session.ts
@@ -4,14 +4,14 @@ export const sessionSchema = z.object({
   id: z.string(),
   sessionDuration: z
     .number()
-    .positive({ message: "informe um valor positivo para seção de estudos." }),
+    .positive({ message: "Enter a positive value for the study section." }),
   date: z.coerce.date(),
   subjectId: z
     .string()
-    .min(5, { message: "informe um id de um assunto valído" }),
+    .min(5, { message: "Please provide a valid subject ID." }),
 });
 
 export const deleteSession = z.object({
-  id: z.string().min(5, { message: "informe um id valído de um assunto." }),
+  id: z.string().min(5, { message: "Please provide a valid session ID." }),
 });
-export const findSession = z.object({id:z.string().min(5, { message: "informe um id valído de um assunto." })})
+export const findSession = z.object({id:z.string().min(5, { message: "Please provide a valid subject ID." })})
